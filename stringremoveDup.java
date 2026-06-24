0be4ba9f3147beb937f02991bc6aede85f8bacad
aarya-1116 <aaryamishra5100@gmail.com>
2026-06-24 14:42:46 +0530

    Day-24 uploaded.

--- stringremoveDup.java
+++ stringremoveDup.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class removeDup {
+public class stringremoveDup {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a string:");
